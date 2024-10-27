434aab7a2f7ae4db60a565dd59ab356badb35c4e
Azi-jsProger <167640817+Azi-jsProger@users.noreply.github.com>
2024-10-27 23:09:54 +0600

    redux

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import styles from './main.module.css'
+import {useDispatch} from "react-redux";
+import {setData} from "../../redux/slices/dataSlices";
+import {useNavigate} from "react-router-dom";
 
 const MainPage = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+function switchClick() {
+    navigate(`/register`)
+    dispatch(setData('Hello'));
+}
+
     return (
         <div>
             Привет
+            <button onClick={switchClick}>Click</button>
         </div>
     );
 };
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './register.module.css'
+import {useSelector} from "react-redux";
 
 const RegisterPage = () => {
+    const text = useSelector(state => state.data);
+
     return (
         <div>
             Register
+            <h1>{text}</h1>
         </div>
     );
 };
